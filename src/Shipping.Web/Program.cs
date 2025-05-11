5cc8e60a973e9cd7300744bd5a424292ed43f888
Vladislav Antonov <vantonov95@yandex.ru>
2025-05-11 19:01:36 +0300

    Add swagger

--- src/Shipping.Web/Program.cs
+++ src/Shipping.Web/Program.cs
@@ -6,10 +6,22 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddApplicationServices();
 
     builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shipping API", Version = "v1" });
+    });
 }
 
 var app = builder.Build();
 {
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
     app.UseHsts();
     app.UseHttpsRedirection();
 
